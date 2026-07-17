c9e05e4a016f45c3a02cabfab6233b865e399883
rimaru06 <shivraodanger04@gmail.com>
2026-07-18 00:40:48 +0530

    chore: created basic login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,25 @@
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {loginSchema} from '../lib/validations';
+import { Button } from '@/components/ui/button';
 function Login()
 {
+    const { register, handleSubmit, formState: {errors} } = useForm({
+        resolver: zodResolver(loginSchema)
+    });
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
     return <>
-        <h1>Login</h1>
+     <form  onSubmit={handleSubmit(onSubmit)}>
+        <input type="email" placeholder='Email' {...register('email')} />
+        <p>{errors.email?.message}</p>
+        <input type="passoword" placeholder='Password' {...register('password')} />
+        <p>{errors.password?.message}</p>
+        <Button type='submit'>Login</Button>
+     </form>
     </>
 }
 
